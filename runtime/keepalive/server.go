103ead4c5c42a2414870c3da74672e37a4ff400e
ganyyy <1354410847@qq.com>
2022-05-13 17:08:12 +0800

    tcp user timeout

--- runtime/keepalive/server.go
+++ runtime/keepalive/server.go
@@ -22,19 +22,19 @@ func RunServer(done context.Context) {
 	var tcpConn, _ = conn.(*net.TCPConn)
 	var f, _ = tcpConn.File()
 	// KEEP ALIVE
-	unix.SetsockoptInt(int(f.Fd()), unix.SOL_SOCKET, unix.SO_KEEPALIVE, 1)
+	_ = unix.SetsockoptInt(int(f.Fd()), unix.SOL_SOCKET, unix.SO_KEEPALIVE, 1)
 
 	// 检查超时
-	unix.SetsockoptInt(int(f.Fd()), unix.IPPROTO_TCP, unix.TCP_KEEPIDLE, 10)
+	_ = unix.SetsockoptInt(int(f.Fd()), unix.IPPROTO_TCP, unix.TCP_KEEPIDLE, 10)
 
 	// 检查间隔
-	unix.SetsockoptInt(int(f.Fd()), unix.IPPROTO_TCP, unix.TCP_KEEPINTVL, 5)
+	_ = unix.SetsockoptInt(int(f.Fd()), unix.IPPROTO_TCP, unix.TCP_KEEPINTVL, 5)
 
 	// 检查次数
-	unix.SetsockoptInt(int(f.Fd()), unix.IPPROTO_TCP, unix.TCP_KEEPCNT, 1)
+	_ = unix.SetsockoptInt(int(f.Fd()), unix.IPPROTO_TCP, unix.TCP_KEEPCNT, 1)
 
 	// 设置USER_TIMEOUT: TCP_KEEPIDLE + TCP_KEEPINTVL * TCP_KEEPCNT
-	unix.SetsockoptInt(int(f.Fd()), unix.IPPROTO_TCP, unix.TCP_USER_TIMEOUT, 15*1000)
+	_ = unix.SetsockoptInt(int(f.Fd()), unix.IPPROTO_TCP, unix.TCP_USER_TIMEOUT, 15*1000)
 
 	// 等待退出
 	<-done.Done()
