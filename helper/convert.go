9f7ccab277da0d40461a77575a347ec55e953731
ganyyy <1354410847@qq.com>
2022-05-17 23:18:26 +0800

    add any decode encode

--- helper/convert.go
+++ helper/convert.go
@@ -1,5 +1,12 @@
 package helper
 
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/golang/protobuf/proto"
+)
+
 func To[T any](v any) T {
 	var ret, ok = v.(T)
 	if !ok {
@@ -7,4 +14,52 @@ func To[T any](v any) T {
 		return rr
 	}
 	return ret
+}
+
+type Data interface {
+	Marshal(interface{}) ([]byte, error)
+	Unmarshal([]byte, interface{}) error
+}
+
+type JsonStruct struct{}
+
+func (d JsonStruct) Marshal(v interface{}) ([]byte, error) {
+	return json.Marshal(v)
+}
+
+func (d JsonStruct) Unmarshal(bs []byte, v interface{}) error {
+	return json.Unmarshal(bs, v)
+}
+
+type ProtoStruct struct{}
+
+func (p ProtoStruct) Marshal(v interface{}) ([]byte, error) {
+	return proto.Marshal(v.(proto.Message))
+}
+
+func (p ProtoStruct) Unmarshal(bs []byte, v interface{}) error {
+	return proto.Unmarshal(bs, v.(proto.Message))
+}
+
+func Load[T any](data Data, path string) (v T, err error) {
+	bs, _ := os.ReadFile(path)
+	err = data.Unmarshal(bs, &v)
+	return
+}
+
+var (
+	jsonData  = JsonStruct{}
+	protoData = ProtoStruct{}
+)
+
+var _ = func() {
+	type S struct {
+		Name string
+	}
+	var t, e = Load[S](jsonData, "path")
+	_ = e
+	_ = t.Name
+	t, e = Load[S](protoData, "path")
+	_ = e
+	_ = t.Name
 }
