51c2248dc1d10007a3e4f23e3b342667939621a6
ganyyy <1354410847@qq.com>
2022-08-03 11:41:14 +0800

    1.19 api

---
+++ api/1.19api/api_test.go
@@ -0,0 +1,90 @@
+package api_119
+
+import (
+	"fmt"
+	"runtime/debug"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFmtAppend(t *testing.T) {
+	var buf [100]byte
+
+	var show = fmt.Appendf(buf[:0], "%s", "hello world") // hello world
+	show = fmt.Appendln(show, "1", "2")                  // hello world1 2
+	show = fmt.Append(show, "3", "4")                    // hello world1 2\n3 4
+
+	t.Logf("%s", show)
+}
+
+func TestMemoryLimit(t *testing.T) {
+	debug.SetMemoryLimit(2 ^ 20) //GB以下不进行GC
+}
+
+type Common struct {
+	Name string
+}
+
+func TestAtomic(t *testing.T) {
+	var pc atomic.Pointer[Common]
+	var ov = &Common{
+		Name: "123",
+	}
+	pc.Store(ov)
+
+	var v = pc.Load()
+
+	assert.Equal(t, v, ov)
+}
+
+func TodayHour(t time.Time, hour int) time.Time {
+	var loc = time.Local
+	return time.Date(t.Year(), t.Month(), t.Day(), hour, 0, 0, 0, loc)
+}
+
+func TestAbs(t *testing.T) {
+	var now = time.Now()
+	var nextDay = now.AddDate(0, 0, 1)
+
+	assert.Equal(t, now.Sub(nextDay).Abs(), nextDay.Sub(now).Abs())
+	t.Logf("sub1:%v, sub2:%v", now.Sub(nextDay).Abs(), nextDay.Sub(now).Abs())
+}
+
+func TestTodayHour(t *testing.T) {
+	var now = time.Now()
+
+	var t1 = TodayHour(now, 5)
+	var t2 = TodayHourMath(now, 5)
+	assert.Equal(t, t1, t2)
+	t.Logf("now:%v, now 5:%v", now, TodayHour(now, 5))
+}
+
+var zero = time.Date(2006, 1, 1, 0, 0, 0, 0, time.Local).Unix()
+
+func TodayHourMath(t time.Time, hour int) time.Time {
+	const HourSecond = 60 * 60
+	const DaySecond = HourSecond * 24
+	var subDay = (t.Unix() - zero) / DaySecond
+	return time.Unix(zero+subDay*DaySecond+HourSecond*int64(hour), 0)
+}
+
+func BenchmarkLocal(b *testing.B) {
+	var now = time.Now()
+	b.Run("time local", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			TodayHour(now, 5)
+		}
+	})
+
+	b.Run("math local", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			TodayHourMath(now, 5)
+		}
+	})
+}
+
+func TestSort(t *testing.T) {
+}
--- runtime/syscall/main.go
+++ runtime/syscall/main.go
@@ -1,4 +1,4 @@
-//go:build ignore
+//go:build linux
 
 package main
 
