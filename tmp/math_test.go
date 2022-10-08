2af701386626644d563425366f14e12e424fff9c
ganyyy <1354410847@qq.com>
2022-10-08 22:26:57 +0800

    math mod

--- tmp/math_test.go
+++ tmp/math_test.go
@@ -29,3 +29,27 @@ func TestMathInf(t *testing.T) {
 	t.Log(math.Inf(0) + math.NaN())
 	t.Log(math.Inf(-1) + 1)
 }
+
+func TestMethMod(t *testing.T) {
+	a, b := 5, -5
+
+	logMod := func(a, b int) {
+		t.Logf("%d %% %d = %d", a, b, a%b)
+	}
+
+	logMod(a, -3)
+	logMod(a, 3)
+	logMod(b, -3)
+	logMod(b, 3)
+
+	const k = 5
+
+	logMod2 := func(i, id int) {
+		v := i + ((id-i)%k+k)%k
+		t.Logf("i:%v, id:%v, nextId:%v", i, id, v)
+	}
+
+	for i := 0; i < 10; i++ {
+		logMod2(i, 3)
+	}
+}
