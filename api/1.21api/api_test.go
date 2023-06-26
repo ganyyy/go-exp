e2e3ee99c406a883350648864e190a9a0d53806a
ganyyy <1354410847@qq.com>
2023-06-27 00:48:16 +0800

    实现带有stack的slog

--- api/1.21api/api_test.go
+++ api/1.21api/api_test.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"math"
 	"os"
+	"runtime"
+	"strings"
 	"testing"
 
 	"log/slog"
@@ -23,22 +25,74 @@ func TestApi(t *testing.T) {
 	require.Equal(t, make([]int, 5), s)
 }
 
+type MyHandler struct {
+	log slog.Handler
+	t   *testing.T
+}
+
+// Enabled implements slog.Handler.
+func (m *MyHandler) Enabled(c context.Context, l slog.Level) bool {
+	return m.log.Enabled(c, l)
+}
+
+// WithAttrs implements slog.Handler.
+func (m *MyHandler) WithAttrs(attrs []slog.Attr) slog.Handler {
+	return &MyHandler{
+		log: m.log.WithAttrs(attrs),
+		t:   m.t,
+	}
+}
+
+// WithGroup implements slog.Handler.
+func (m *MyHandler) WithGroup(name string) slog.Handler {
+	return &MyHandler{
+		log: m.log.WithGroup(name),
+		t:   m.t,
+	}
+}
+
+func (m *MyHandler) Handle(ctx context.Context, r slog.Record) error {
+	// TODO 如果要实现边长的stack?
+	if r.Level < slog.LevelError {
+		return m.log.Handle(ctx, r)
+	}
+	// 更好的实现, 可以参考zap的实现, 这里只是简单的展示一个例子
+	var pcs [16]uintptr
+	n := runtime.Callers(4, pcs[:])
+	frames := runtime.CallersFrames(pcs[:n])
+	var stack []string
+	for {
+		frame, more := frames.Next()
+		stack = append(stack, frame.Function)
+		if !more {
+			break
+		}
+	}
+	r.AddAttrs(slog.String("stack", strings.Join(stack, "\n")))
+	return m.log.Handle(ctx, r)
+}
+
+func dfsCall(i int) int {
+	if i < 10 {
+		slog.Debug("dfs call", slog.Int("i", i))
+		return dfsCall(i + 1)
+	}
+	slog.Error("dfs call end!", slog.Int("i", i))
+	return i
+}
+
 func TestSLog(t *testing.T) {
-	defer func() {
-		_ = os.Stdout.Sync()
-	}()
-	var l = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
-		AddSource: true,
-		Level:     slog.LevelDebug,
-		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-			_ = groups
-			return a
-		},
-	}))
+	defer func() { _ = os.Stdout.Sync() }()
+	var l = slog.New(&MyHandler{
+		t: t,
+		log: slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			AddSource: true,
+			Level:     slog.LevelDebug,
+		}),
+	})
 
-	l.Error("hello %s, %d, %s", "world", 123, "abc")
-
-	l.LogAttrs(context.Background(), slog.LevelDebug, "hello", slog.String("name", "123"), slog.Int("age", 10))
+	l = l.WithGroup("test").With(slog.String("name", "123"), slog.Int("age", 10))
+	l.Debug("group test", slog.String("name", "456"))
 	slog.SetDefault(l)
-	slog.Debug("hello %s, %d, %s", "world", 123, "abc")
+	dfsCall(0)
 }
